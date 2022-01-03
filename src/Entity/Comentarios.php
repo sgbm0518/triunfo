35c647b8fc0fdb6405fbbe4b817b8e7a3f0e606c
Sergio Madrid <sgbm0518@gmail.com>
2022-01-02 21:15:46 -0500

    symfony 5 course

---
+++ src/Entity/Comentarios.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ComentariosRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ComentariosRepository::class)
+ */
+class Comentarios
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $comentario;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $fecha_publicacion;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getComentario(): ?string
+    {
+        return $this->comentario;
+    }
+
+    public function setComentario(?string $comentario): self
+    {
+        $this->comentario = $comentario;
+
+        return $this;
+    }
+
+    public function getFechaPublicacion(): ?\DateTimeInterface
+    {
+        return $this->fecha_publicacion;
+    }
+
+    public function setFechaPublicacion(\DateTimeInterface $fecha_publicacion): self
+    {
+        $this->fecha_publicacion = $fecha_publicacion;
+
+        return $this;
+    }
+}
