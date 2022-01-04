31ee5c94ab364d685bd771d9b503969c69420522
Sergio Madrid <sgbm0518@gmail.com>
2022-01-03 21:23:38 -0500

    subida de archivos

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -8,18 +8,43 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+
 
 class PostsController extends AbstractController
 {
     /**
      * @Route("/registrar-posts", name="RegistrarPosts")
      */
-    public function index(Request $request)
+    public function index(Request $request, SluggerInterface $slugger)
     {
         $post = new Posts();
         $form = $this->createForm(PostsType::class, $post);
         $form->handleRequest($request);
         if($form->isSubmitted() && $form->isValid()){
+            $brochureFile = $form['foto']->getData();
+            if ($brochureFile) {
+                $originalFilename = pathinfo($brochureFile->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$brochureFile->guessExtension();
+
+                // Move the file to the directory where brochures are stored
+                try {
+                    $brochureFile->move(
+                        $this->getParameter('photos_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    throw new \Exception('UPs! ha ocurrido un error, sorry :(');
+                }
+
+                // updates the 'brochureFilename' property to store the PDF file name
+                // instead of its contents
+                $post->setFoto($newFilename);
+            }
             $user = $this->getUser(); // Obtener al usuario loqueado, que esta actualmente logueado en mi sistema
             // $post->setUser($user); // editar el post, le estamos diciendo que edite el usuario, y el usuario es el que acabamos de obtener ($user)
             $em = $this->getDoctrine()->getManager(); // estas 3 lineas de codigo que estan siendo comentariadas estan guardando mi posts en la base de datos
